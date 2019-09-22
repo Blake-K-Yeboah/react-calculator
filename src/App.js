6e4ae4ca6cf7a3da586561cc048db7f32017518a
Blake Yeboah <blakekyeboah@gmail.com>
2019-09-22 20:28:08 +1000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 function App() {
+
+  const [output, setOutput] = useState('');
+
+  const appendOutput = (val) => {
+    setOutput(output + val.toString());
+  }
+
+  const clearOutput = () => {
+    setOutput('');
+  }
+
+  const calculateAnswer = () => {
+    let answer = eval(output);
+
+    setOutput(answer);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <h1>Calculator</h1>
+        <div className="output">{output}</div>
+        <div className="btn-grid">
+          <button onClick={appendOutput.bind(this, 7)}>7</button>
+          <button onClick={appendOutput.bind(this, 8)}>8</button>
+          <button onClick={appendOutput.bind(this, 9)}>9</button>
+          <button className="dark" onClick={appendOutput.bind(this, ' / ')}>&divide;</button>
+          <button className="red" onClick={clearOutput}>&times;</button>
+          <button onClick={appendOutput.bind(this, 4)}>4</button>
+          <button onClick={appendOutput.bind(this, 5)}>5</button>
+          <button onClick={appendOutput.bind(this, 6)}>6</button>
+          <button className="dark" onClick={appendOutput.bind(this, ' * ')}>&times;</button>
+          <button className="dark" onClick={appendOutput.bind(this, ' / 100')}>%</button>
+          <button onClick={appendOutput.bind(this, 1)}>1</button>
+          <button onClick={appendOutput.bind(this, 2)}>2</button>
+          <button onClick={appendOutput.bind(this, 3)}>3</button>
+          <button className="dark" onClick={appendOutput.bind(this, ' - ')}>&minus;</button>
+          <button className="orange" onClick={calculateAnswer}>=</button>
+          <button className="column2" onClick={appendOutput.bind(this, 0)}>0</button>
+          <button className="dot" onClick={appendOutput.bind(this, '.')}>.</button>
+          <button className="dark" onClick={appendOutput.bind(this, ' + ')}>+</button>
+        </div>
+      </div>
     </div>
   );
 }
